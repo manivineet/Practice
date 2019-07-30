5db57c299d6d46adf294eddca4a01b3f7edabe30
manivineet <manivineet@gmail.com>
2019-07-30 12:39:47 +1000

    creating the list page

--- ASP.NET Core/MVC/BethanysPieShop/Controllers/HomeController.cs
+++ ASP.NET Core/MVC/BethanysPieShop/Controllers/HomeController.cs
@@ -5,14 +5,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 
 namespace BethanysPieShop.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+             var pies = _pieRepository
+                        .GetAllPies()
+                        .OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Pies = pies.ToList(),
+                Title = "Welcome to Bethany's Pie Shop"
+            };
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
--- ASP.NET Core/MVC/BethanysPieShop/Startup.cs
+++ ASP.NET Core/MVC/BethanysPieShop/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -24,6 +25,8 @@ namespace BethanysPieShop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPieRepository, MockPieRepository>();
+
             services.AddMvc();
 
             services.Configure<CookiePolicyOptions>(options =>
